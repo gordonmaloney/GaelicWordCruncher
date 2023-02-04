f6850ea35f38b018d812044e5c9a2ca0955a08e4
gordonmaloney <gordonmaloney@gmail.com>
2023-02-04 14:13:07 +0000

    routes changed to push to handle accents

--- index.py
+++ index.py
@@ -14,27 +14,32 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
-    return('API running - routes: /gender/{word}, /ipa/{word}, /audio/{word}, /all/{word}' )
+    return('API running - woohoo\n\nroutes:\n/gender\n/ipa\n/audio\n/all' )
 
 
-@app.route('/gender/<word>', methods=['GET'])
-def genderroute(word):
+@app.route('/gender', methods=['POST'])
+def genderroute():
+    word = request.form.get('word')
     return gender.FetchGender(word)
 
 
-@app.route('/ipa/<word>', methods=['GET'])
-def iparoute(word):
+@app.route('/ipa', methods=['POST'])
+def iparoute():
+    word = request.form.get('word')
     return ipa.FetchIpa(word)
 
 
-@app.route('/audio/<word>', methods=['GET'])
-def audioroute(word):
+@app.route('/audio', methods=['POST'])
+def audioroute():
+    word = request.form.get('word')
     return audio.FetchAudio(word)
 
 
-@app.route('/all/<word>', methods=['GET'])
-def allroutes(word):
+@app.route('/all', methods=['POST'])
+def allroutes():
+    word = request.form.get('word')
     return jsonify(
+        word = word,
         gender = gender.FetchGender(word),
         audio = audio.FetchAudio(word),
         ipa = ipa.FetchIpa(word)
